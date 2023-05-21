76e5062586c6a14fc390ddb9e93a2ea9e24cb434
Lateralus138 <faithnomoread@yahoo.com>
2023-05-20 21:26:32 -0500

    Added boxunichars - 05/21/2023 02:26:31 UTC

--- boxunichars/boxunichars/boxunichars.cpp
+++ boxunichars/boxunichars/boxunichars.cpp
@@ -1,7 +1,7 @@
 ﻿//////////////////////////////////////////////////
-// Box Unicode Characters - Print all the ASCII //
-// box characters with their corresponding Alt  //
-// codes.                                       //
+// Box Unicode Characters - Print all the box   //
+// characters with their corresponding alt and  //
+// hexadecimal codes.                           //
 // © 2023 Ian Pride - New Pride Software /      //
 // Services                                     //
 //////////////////////////////////////////////////
@@ -12,8 +12,9 @@
 #include <chrono>
 #include <thread>
 #include "BoxUniCharsLib.h"
+#include "BoxUniCharsRegex.h"
 
-int main()
+int main(int argc, const char* argv[])
 {
   HANDLE hInput = GetStdHandle(STD_INPUT_HANDLE);
   SetConsoleMode(hInput, ENABLE_VIRTUAL_TERMINAL_INPUT);
@@ -33,27 +34,109 @@ int main()
       SendMessageW(HWND_, WM_SETICON, ICON_SMALL, (LPARAM)hIcon);
     }
   }
-
-  std::string output = "";
-
-  BoxUniCharsLib box;
-
-  box.SetAll(false);
-  output = box.Default();
-  std::cout << output;
-  output = box.Horizontal();
-  std::cout << output;
-  output = box.Vertical();
-  std::cout << output;
-  output = box.SingleVerticalDoubleHorizontal();
-  std::cout << output;
-  output = box.DoubleVerticalSingleHorizontal();
-  std::cout << output;
-  output = box.Single();
-  std::cout << output;
-  output = box.Double();
-  std::cout << output;
-  
+  std::string boxOutput;
+  if (argc > 1)
+  {
+    bool mode = true; // alt
+    BoxUniCharsLib box;
+    const std::vector<std::string> args(argv + 1, argv + argc);
+    for (const std::string arg : args)
+    {
+      if (std::regex_match(arg, BoxUniCharsRegex::HELP))
+      {
+        const std::string HELPMESSAGE =
+          "\n"
+          "Box Unicode Characters - Print all box characters\n"
+          "                         with their corresponding\n"
+          "                         alt andhexadecimal codes.\n"
+          "\n"
+          "boxunichars   [/? | /h | /help] | [/a | /all] |\n"
+          "  [/z | /horizonatal] | [/v | /vertical]\n"
+          "  [/s | /single] | [/d | /double]\n"
+          "  [/sd | /singledouble] | [/ds | /doublesingle]\n"
+          "  [/x | /hexadecimal] | [/l | /alt]\n"
+          "\n"
+          "  The user can use as many of these display switches\n"
+          "  as they like and they can change the code mode (alt\n"
+          "  or hexadecimal) of each by providing either the\n"
+          "  /l, /alt, /x, or /hexadecimal switches before each\n"
+          "  display switch. The last provided code mode will be\n"
+          "  be used if none is provided (defaulting to alt\n"
+          "  mode)\n"
+          "\n"
+          "  /a, /all            Display all types in a square.\n"
+          "  /z, /horizontal     Display all types horizontally.\n"
+          "  /v, /vertical       Display all types vertically.\n"
+          "  /s, /single         Display single type.\n"
+          "  /d, /double         Display double type.\n"
+          "  /sd, /singledouble  Display single vertical double\n"
+          "                      horizontal type.\n"
+          "  /ds, /doublesingle  Display double vertical single\n"
+          "                      horizontal type.\n"
+          "  /x, /hexadecimal    Values displayed in hexadecimal\n"
+          "                      code format.\n"
+          "  /l, /alt            Values displayed in alt code\n"
+          "                      format. This is the default,\n"
+          "\n";
+        std::cout << HELPMESSAGE;
+        WaitCheck();
+        return EXIT_SUCCESS;
+      }
+      if (std::regex_match(arg, BoxUniCharsRegex::ALT))
+      {
+        mode = true;
+        continue;
+      }
+      if (std::regex_match(arg, BoxUniCharsRegex::HEXADECIMAL))
+      {
+        mode = false;
+        continue;
+      }
+      if (std::regex_match(arg, BoxUniCharsRegex::ALL))
+      {
+        box.SetAllDefault(mode);
+        boxOutput.append(box.Default());
+        continue;
+      }
+      if (std::regex_match(arg, BoxUniCharsRegex::HORZ))
+      {
+        box.SetAllHorz(mode);
+        boxOutput.append(box.Horizontal());
+        continue;
+      }
+      if (std::regex_match(arg, BoxUniCharsRegex::VERT))
+      {
+        box.SetAllVert(mode);
+        boxOutput.append(box.Vertical());
+        continue;
+      }
+      if (std::regex_match(arg, BoxUniCharsRegex::SINGLE))
+      {
+        box.SetSingle(mode);
+        boxOutput.append(box.Single());
+        continue;
+      }
+      if (std::regex_match(arg, BoxUniCharsRegex::DOUBLE))
+      {
+        box.SetDouble(mode);
+        boxOutput.append(box.Double());
+        continue;
+      }
+      if (std::regex_match(arg, BoxUniCharsRegex::SINGLEDOUBLE))
+      {
+        box.SetSingleDouble(mode);
+        boxOutput.append(box.SingleVerticalDoubleHorizontal());
+        continue;
+      }
+      if (std::regex_match(arg, BoxUniCharsRegex::DOUBLESINGLE))
+      {
+        box.SetDoubleSingle(mode);
+        boxOutput.append(box.DoubleVerticalSingleHorizontal());
+        continue;
+      }
+    }
+  }
+  std::cout << boxOutput;
   WaitCheck();
   return EXIT_SUCCESS;
 }
