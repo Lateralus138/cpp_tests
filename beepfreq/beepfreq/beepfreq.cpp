b34ec7b1f45c1c8bbaf2f77fd16aae5ae5a4dd2f
Lateralus138 <faithnomoread@yahoo.com>
2023-06-17 21:34:37 -0500

    Added beepfreq - 06/18/2023 02:34:35 UTC

---
+++ beepfreq/beepfreq/beepfreq.cpp
@@ -0,0 +1,15 @@
+﻿// ╔═══════════════════════════════════════════════════════════╗
+// ║ 'Beep Frequency' - Execute a beep on a Windows computer.  ║
+// ║ © 2023 Ian Pride - New Pride Software / Services          ║
+// ╚═══════════════════════════════════════════════════════════╝
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include "ArgParser.h"
+int main(int argc, const char* argv[])
+{
+  Parameters params;
+  if (argc > 1) ArgParser(argc, argv, params);
+  const bool beep_status = Beep(params.beep_frequency, params.beep_duration);
+  if (!beep_status) params.error_value = 5;
+  return params.error_value;
+}
--- boxunichars/boxunichars/Globals.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#ifndef GLOBALS_H
-#define GLOBALS_H
-#include <string>
-void WaitCheck();
-#endif // !GLOBALS_H
-
