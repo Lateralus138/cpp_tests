29abf8704117f812f5daaba968a1c17c6a357586
Lateralus138 <faithnomoread@yahoo.com>
2023-07-13 23:57:15 -0500

    Updated CPPPROJ for Linux - 1689310635

--- cppproj/src/include/Functions.cpp
+++ cppproj/src/include/Functions.cpp
@@ -71,3 +71,108 @@ bool CreateFile(std::filesystem::path path, std::string content, Parameters &par
   }
   return file_created;
 }
+int process_directories(std::map<std::string, std::filesystem::path> &dir_paths, Parameters &params)
+{
+  for (std::pair const [key, value] : dir_paths)
+  {
+    if (key == "source") continue;
+    std::string temp_message = "Checking if ";
+    const std::string path = value.native().c_str();
+    temp_message.append(path);
+    temp_message.append(" exists...\n");
+    std::cout << temp_message;
+    temp_message.clear();
+    const bool dir_exists = PathExists(value, params, 7);
+    if (params.error_value > 0)
+    {
+      return params.error_value;
+    }
+    temp_message.append(path);
+    StringAppendIf(dir_exists, temp_message,
+                   " already exists and does not need to be created...\n",
+                   " does not exist; attempting to create directory...\n");
+    std::cout << temp_message;
+    temp_message.clear();
+    if (dir_exists) continue;
+    const bool dir_created = CreateDirectory(value, params, 8);
+    if (params.error_value > 0)
+    {
+      return params.error_value;
+    }
+    temp_message.append(path);
+    temp_message.append(" was created successfully...\n");
+    std::cout << temp_message;
+    temp_message.clear();
+  }
+  return params.error_value;
+}
+int process_files
+(
+  std::map<std::string, std::filesystem::path> & file_paths,
+  Parameters &params, Globals &globals, Switches & switches
+)
+{
+  for (std::pair const [key, value] : file_paths)
+  {
+    std::string temp_message = "Checking if ";
+    const std::string path = value.native().c_str();
+    temp_message.append(path);
+    temp_message.append(" exists...\n");
+    std::cout << temp_message;
+    temp_message.clear();
+    const bool file_exists = PathExists(value, params, 9);
+    if (params.error_value > 0)
+    {
+      return params.error_value;
+    }
+    temp_message.append(path);
+    StringAppendIf(file_exists, temp_message,
+                   " already exists and does not need to be created...\n",
+                   " does not exist; attempting to create file...\n");
+    std::cout << temp_message;
+    temp_message.clear();
+    if (file_exists) continue;
+    std::string content;
+    if (key == "main")
+    {
+      content = globals.main_content;
+    }
+    else if (key == "make")
+    {
+      content = globals.makefile_content;
+    }
+    else if (key == "pch_h")
+    {
+      content = globals.pch_content;
+    }
+    else if ((switches.prebuild_script) && (key == "prebuild"))
+    {
+      content = globals.prebuild_content;
+    }
+    else
+    {
+      bool delim_found = false;
+      size_t delim_pos = key.find('_');
+      delim_found = (delim_pos != std::string::npos);
+      if (!delim_found)
+      {
+        continue;
+      }
+      std::string ext_ = key.substr(delim_pos);
+      if (ext_ == "_h")
+      {
+        content = globals.class_header_content_create(key);
+      }
+      else
+      {
+        content = globals.class_source_content_create(value.stem().native().c_str());
+      }
+    }
+    CreateFile(value, content, params, 10);
+    if (params.error_value > 0)
+    {
+      return params.error_value;
+    }
+  }
+  return params.error_value;
+}
--- cppproj/src/main.cpp
+++ cppproj/src/main.cpp
@@ -65,98 +65,11 @@ int main(int argc, const char *argv[])
   std::map<std::string, std::filesystem::path> file_paths = files.getPaths();
   globals.pch_content_create(params);
   globals.makefile_content_create(params);
-  for (std::pair const [key, value] : dir_paths)
+  const int dir_create_result = process_directories(dir_paths, params);
+  if (dir_create_result > 0)
   {
-    if (key == "source") continue;
-    std::string temp_message = "Checking if ";
-    const std::string path = value.native().c_str();
-    temp_message.append(path);
-    temp_message.append(" exists...\n");
-    std::cout << temp_message;
-    temp_message.clear();
-    const bool dir_exists = PathExists(value, params, 7);
-    if (params.error_value > 0)
-    {
-      return params.error_value;
-    }
-    temp_message.append(path);
-    StringAppendIf(dir_exists, temp_message,
-                   " already exists and does not need to be created...\n",
-                   " does not exist; attempting to create directory...\n");
-    std::cout << temp_message;
-    temp_message.clear();
-    if (dir_exists) continue;
-    const bool dir_created = CreateDirectory(value, params, 8);
-    if (params.error_value > 0)
-    {
-      return params.error_value;
-    }
-    temp_message.append(path);
-    temp_message.append(" was created successfully...\n");
-    std::cout << temp_message;
-    temp_message.clear();
+    return params.error_value;
   }
-  for (std::pair const [key, value] : file_paths)
-  {
-    std::string temp_message = "Checking if ";
-    const std::string path = value.native().c_str();
-    temp_message.append(path);
-    temp_message.append(" exists...\n");
-    std::cout << temp_message;
-    temp_message.clear();
-    const bool file_exists = PathExists(value, params, 9);
-    if (params.error_value > 0)
-    {
-      return params.error_value;
-    }
-    temp_message.append(path);
-    StringAppendIf(file_exists, temp_message,
-                   " already exists and does not need to be created...\n",
-                   " does not exist; attempting to create file...\n");
-    std::cout << temp_message;
-    temp_message.clear();
-    if (file_exists) continue;
-    std::string content;
-    if (key == "main")
-    {
-      content = globals.main_content;
-    }
-    else if (key == "make")
-    {
-      content = globals.makefile_content;
-    }
-    else if (key == "pch_h")
-    {
-      content = globals.pch_content;
-    }
-    else if ((switches.prebuild_script) && (key == "prebuild"))
-    {
-      content = globals.prebuild_content;
-    }
-    else
-    {
-      bool delim_found = false;
-      size_t delim_pos = key.find('_');
-      delim_found = (delim_pos != std::string::npos);
-      if (!delim_found)
-      {
-        continue;
-      }
-      std::string ext_ = key.substr(delim_pos);
-      if (ext_ == "_h")
-      {
-        content = globals.class_header_content_create(key);
-      }
-      else
-      {
-        content = globals.class_source_content_create(value.stem().native().c_str());
-      }
-    }
-    CreateFile(value, content, params, 10);
-    if (params.error_value > 0)
-    {
-      return params.error_value;
-    }
-  }
+  const int file_create_result = process_files(file_paths, params, globals, switches);
   return params.error_value;
 }
