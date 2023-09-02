8dd48d24038c43a170693cfbaebfc831e5c646ce
Lateralus138 <faithnomoread@yahoo.com>
2023-09-01 23:50:37 -0500

    Added hostscompress project - 09/02/2023 04:50:36 UTC

--- nomedia/src/include/Pipe.h
+++
@@ -1,6 +0,0 @@
-#ifndef PIPE_H
-#define PIPE_H
-#include <vector>
-#include <string>
-void VectorFromPipe(std::vector<std::string> &vector, bool skipEmpty);
-#endif // PIPE_H
--- nomedia/src/include/pch.h
+++
@@ -1,7 +0,0 @@
-#ifndef PCH_H
-#define PCH_H
-#include "Pipe.h"
-#include "ArgParser.h"
-#include "Bench.h"
-#include "Functions.h"
-#endif // PCH_H
