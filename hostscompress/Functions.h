b5ab2067c8329c057c7c228d7d40c499c59a4599
Lateralus138 <faithnomoread@yahoo.com>
2025-11-03 17:23:33 -0600

    Added Table Of Contents. - 2025-11-03 23:23:32 UTC

--- hostscompress/Functions.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-#include <vector>
-#include "Options.h" // Include Options.h for PrintMessage
-
-const std::string GetWindowsDirectoryAsString(ProgramError &perror, int errorValue, std::string errorMessage);
-std::string ColorString(std::string string, int color, Options& options); // Declaration for ColorString
-void PrintMessage(std::string message, Options& options);
-void PrintMessage(std::vector<std::string> strings, Options& options);
